f9c53ca9dae45874a775274835c95d46818b2446
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2022-04-13 04:50:08 -0500

    Fix nombre de variable erróneo

--- src/resources/views/versiones/_lista_con_radios.blade.php
+++ src/resources/views/versiones/_lista_con_radios.blade.php
@@ -1,8 +1,8 @@
 
 
 @foreach($ramas as $idx => $rama)
-    <label for="rama-{{$idx}}" class="{{(substr($rama, strpos($rama, $rama_original)) == $rama_original) ? "text-success" : ''}}">
-        <input type="radio" name="rama" id="rama-{{$idx}}" value="{{$rama}}" {{(substr($rama, strpos($rama, $rama_original)) == $rama_original) ? "checked":''}}> {{$rama}}
+    <label for="rama-{{$idx}}" class="{{(substr($rama, strpos($rama, $rama_actual)) == $rama_actual) ? "text-success" : ''}}">
+        <input type="radio" name="rama" id="rama-{{$idx}}" value="{{$rama}}" {{(substr($rama, strpos($rama, $rama_actual)) == $rama_actual) ? "checked":''}}> {{$rama}}
     </label>
     <br>
 @endforeach
