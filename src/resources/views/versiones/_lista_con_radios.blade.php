f65597f41fcb15657446b2ef54512d489cd1faf7
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2022-04-13 04:40:10 -0500

    Fix typo

--- src/resources/views/versiones/_lista_con_radios.blade.php
+++ src/resources/views/versiones/_lista_con_radios.blade.php
@@ -2,7 +2,7 @@
 
 @foreach($ramas as $idx => $rama)
     <label for="rama-{{$idx}}" class="{{(substr($rama, strpos($rama, $rama_original)) == $rama_original) ? "text-success" : ''}}">
-        <input type="radio" name="rama" id="rama-{{$idx}}" value="{{$rama}}" {{substr($rama, strpos($rama, $rama_original)) == $rama_original;) ? "checked":''}}> {{$rama}}
+        <input type="radio" name="rama" id="rama-{{$idx}}" value="{{$rama}}" {{substr($rama, strpos($rama, $rama_original)) == $rama_original) ? "checked":''}}> {{$rama}}
     </label>
     <br>
 @endforeach
